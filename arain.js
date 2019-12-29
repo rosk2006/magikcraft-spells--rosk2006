eae528e1457033bfc4058c41caa45b4748afffe1
rosk2006 <rosk2006@icloud.com>
2019-12-29 14:14:18 +1000

    Update arain.js

--- arain.js
+++ arain.js
@@ -4,7 +4,7 @@ var magik = magikcraft.io;
 //
 //TODO: is runif better than rnom?
 
-function arain(radius, typ){
+function main(radius, typ){
     radius = parseInt(radius) || 20;
     typ = typ || "ANVIL";
     
@@ -12,35 +12,35 @@ function arain(radius, typ){
         rndAnvil(radius, typ);
     }
 }
-function rndAnvil(radius,typ) {
+function main(radius,typ) {
     var height = radius;
     rndBlock(radius, height).setType(org.bukkit.Material[typ])
 }
 // Standard Normal variate using Box-Muller transform.
 //http://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
-function randn_bm() {
+function main() {
     var u = 1 - Math.random(); // Subtraction to flip [0, 1) to (0, 1].
     var v = 1 - Math.random();
     return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
 }
 
 
-function rnorm_unit(count) {
+function main(count) {
     var ret = 0;
     for (var i = 0; i < count; i++) {
         ret += Math.random();
     }
     return ret/count;
 }
-function rnorm(mean, sd) {
+function main(mean, sd) {
     return randn_bm() * sd + mean;
 }
-function runif(from, to) {
+function main(from, to) {
     return scale(Math.random(), from, to);
 }
-function scale(r, from, to) {
+function main(r, from, to) {
     return Math.floor(r*(to - from)+from)
 }
-function rndBlock(radius, height) {
+function main(radius, height) {
     return magik.getSender().location.getBlock().getRelative(rnorm(0, radius/5), height+runif(0, radius*3), rnorm(0, radius/5));
 }
